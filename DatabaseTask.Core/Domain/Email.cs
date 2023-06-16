e095e8feb45c5f6e829a76e26f8476d71ffd8201
TTHK\opilane <darjakrivodonova@gmail.com>
2023-06-16 12:54:51 +0300

    Add project files.

---
+++ DatabaseTask.Core/Domain/Email.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseTask.Core.Domain
+{
+    public class Email
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+        public string? Comment { get; set; }
+    }
+}
